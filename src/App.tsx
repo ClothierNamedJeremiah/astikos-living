60eaca0529be3b386e3b41accd23cad5848ef47f
Sierra Battan <sierra@battan.com>
2021-04-24 16:38:59 -0700

    add unit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React, { ReactElement } from 'react';
 
 import { units } from './api/sample.json';
-import FloorPlan from './enums/FloorPlan';
+import UnitCard from './components/UnitCard';
 import { Unit } from './interfaces/unit.interface';
+
+import './App.scss';
 
 const App : React.FC = (): ReactElement => (
   <main>
-    <h1 style={{ marginBottom: '36px' }}>Astikós Living</h1>
+    <h1 className="title">Astikós Living</h1>
     <h4>Do you want to live at <a href="https://www.astikoslofts.com/">Astikós Lofts</a>?</h4>
     <p>Please explore this website to view the details of all their units.</p>
-    <button style={{ margin: '24px 0px' }} type="button">
+    <button className="floor-plan-button" type="button">
       Select a Floor Plan
     </button>
     {units.sort((unitA: Unit, unitB: Unit): number => unitA.floorplanId - unitB.floorplanId)
-      .map((u: Unit): ReactElement => (
-        <>
-          <p style={{ fontWeight: 'bold', marginBottom: '-12px' }}>{FloorPlan[u.floorplanId]} - {u.name}</p>
-          <p style={{ marginLeft: '6px' }}>{u.vacantDate?.split(' ')[0] || u.leaseStatus} - {u.rent || 'none'}</p>
-        </>
-      ))}
+      .map((u: Unit): ReactElement => <UnitCard unit={u} />)}
   </main>
 );
 
---
+++ src/components/UnitCard.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+import FloorPlan from '../enums/FloorPlan';
+import { Unit } from '../interfaces/unit.interface';
+
+const UnitCard: React.FC<{ unit: Unit }> = ({ unit }): ReactElement => unit && (
+  <>
+    <p style={{ fontWeight: 'bold', marginBottom: '-12px' }}>{FloorPlan[unit.floorplanId]} - {unit.name}</p>
+    <p style={{ marginLeft: '6px' }}>{unit.vacantDate?.split(' ')[0] || unit.leaseStatus} - {unit.rent || 'none'}</p>
+  </>
+);
+
+export default UnitCard;
