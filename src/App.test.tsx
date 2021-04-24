b3b8e34ee86a288a74fcbda0a57cde2438849eb1
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-04-24 11:29:53 -0700

    removing some boilerplate code from create-react-app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Astikós Living/i);
   expect(linkElement).toBeInTheDocument();
 });
